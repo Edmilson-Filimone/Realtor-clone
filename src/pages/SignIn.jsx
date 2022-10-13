53ac746e8462824c18231c84bccc69838a23c92c
Edmilson-Filimone <philimone99@gmail.com>
2022-10-13 20:48:16 +0200

    Implementing signIn function (sign in  with email and password) on the Sign In page

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,23 +1,50 @@
+import { async } from "@firebase/util";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
-import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai'
-import { Link } from "react-router-dom";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import OAuthButton from "../components/OAuthButton";
 
 const SignIn = () => {
-
   /** usestate for handle inputs data */
-  const [passwordVisibility, setPasswordVisibility] = useState(false)
+  const [passwordVisibility, setPasswordVisibility] = useState(false);
   const [inputData, setInputData] = useState({ email: "", password: "" });
-  const {email, password} = inputData  // destructing the object {} to retrieve individuals values
-  
+  const { email, password } = inputData; // destructing the object {} to retrieve individuals values
+
+  /** useNavigate */
+  const navigate = useNavigate();
+
   /** onchange functions for handle input changes during onChange event */
   const onchange = (e) => {
     setInputData((preStat) => ({
       ...preStat,
       [e.target.id]: e.target.value,
     }));
-    console.log(`Your email: ${email} and Your password: ${password}`)
+    console.log(`Your email: ${email} and Your password: ${password}`);
   };
+
+  /** Sign in with email and password provider */
+  async function signIn_button() {
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+
+      if (user) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error(
+        "You have no access to continue, please consider to check your credential or register"
+      );
+    }
+  }
 
   return (
     <section className="py-5 px-10 md:px-24 lg:px-36 xl:px-44">
@@ -48,19 +75,37 @@ const SignIn = () => {
             onChange={onchange}
           />
           <div className="absolute top-[87px] right-3 text-gray-400">
-            {!passwordVisibility && (<AiFillEyeInvisible onClick={() => setPasswordVisibility(true)} />)}
-            {passwordVisibility && (<AiFillEye onClick={() => setPasswordVisibility(false)} />)}
+            {!passwordVisibility && (
+              <AiFillEyeInvisible onClick={() => setPasswordVisibility(true)} />
+            )}
+            {passwordVisibility && (
+              <AiFillEye onClick={() => setPasswordVisibility(false)} />
+            )}
           </div>
           <div className="flex justify-between py-4">
             <div>
               <span>Don't have an account?</span>
-              <Link to={"/sign-up"} className="px-1 text-red-500 cursor-pointer">Register</Link>
+              <Link
+                to={"/sign-up"}
+                className="px-1 text-red-500 cursor-pointer"
+              >
+                Register
+              </Link>
             </div>
-            <Link to={"/forgot-password"} className="text-blue-500 cursor-pointer">
+            <Link
+              to={"/forgot-password"}
+              className="text-blue-500 cursor-pointer"
+            >
               Forgot password?
             </Link>
           </div>
-          <button className="w-full px-5 py-2 shadow-md rounded-md text-white text-center uppercase font-medium bg-blue-500 hover:brightness-75">
+          <button
+            className="w-full px-5 py-2 shadow-md rounded-md text-white text-center uppercase font-medium bg-blue-500 hover:brightness-75"
+            onClick={(e) => {
+              e.preventDefault();
+              signIn_button();
+            }}
+          >
             Sign In
           </button>
           <div className="flex items-center py-4">
