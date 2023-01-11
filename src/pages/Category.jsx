7cd147d3fa987648133bf206b9d1adcb04f0df50
Edmilson-Filimone <philimone99@gmail.com>
2023-01-12 00:15:29 +0200

    Creating and Styling the card of category page and adjusting the max width of Header component

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -11,7 +11,8 @@ import SwiperCore, {
   Pagination,
 } from "swiper";
 import "swiper/css/bundle";
-import { FaShare } from "react-icons/fa";
+import { FaBath, FaBed, FaChair, FaParking, FaShare } from "react-icons/fa";
+import { HiLocationMarker } from "react-icons/hi";
 
 function Category() {
   const params = useParams();
@@ -68,6 +69,56 @@ function Category() {
       </Swiper>
       <div className="absolute z-10 top-[60px] right-[10px] text-lg text-slate-500 bg-white p-3.5 rounded-full" title="Copy link" onClick={copyShare}><FaShare/></div>
       {copied && (<div className="absolute z-10 top-[65px] right-[60px] text-sm bg-sky-300 p-2 rounded-full">Copied</div>)}
+      <div className="flex flex-col md:flex-row max-w-6xl mx-auto mt-4 mb-6 py-9 px-4 bg-white rounded-md shadow-md">
+        <div className="w-full">
+          <h3 className="text-xl text-sky-900 font-bold pb-2.5">
+            <span>{data.name} - </span>
+            {
+             data.offers ? data.discount.toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD",
+              }):
+              data.price.toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD",
+              })
+            }
+            <span>{data.type.includes("rent")?" / Month":" "}</span>
+          </h3>
+        <div className="flex justify-start items-center space-x-2 mb-2.5" title="Location">
+          <span className="text-green-600">
+            <HiLocationMarker />
+          </span>
+          <span className="text-base text-slate-800 font-semibold truncate">
+            {data.address}
+          </span>
+        </div>
+        <div className="w-[180px] bg-red-800 rounded-md shadow-md p-1 mb-2.5 text-center text-base text-white font-semibold capitalize">{`For ${data.type}`}</div>
+        <div className="mb-2">
+          <span className="font-semibold text-slate-800">Description</span>
+          <span className="text-slate-800 pb-1">{` - ${data.description}`}</span>
+        </div>
+        <ul className="flex justify-start space-x-8">
+          <li className="flex space-x-2 items-center text-sm font-semibold ">
+            <FaBed />
+            <span>{`${data.beds} ${data.beds > 1 ? "Beds" : "Bed"}`}</span>
+          </li>
+          <li className="flex space-x-2 items-center text-sm font-semibold ">
+            <FaBath />
+            <span>{`${data.baths} ${data.baths > 1 ? "Baths" : "Bath"}`}</span>
+          </li>
+          {data.parking && (<li className="flex space-x-2 items-center text-sm font-semibold ">
+            <FaParking />
+            <span>Parking Spot</span>
+          </li>)}
+          {data.furnished && (<li className="flex space-x-2 items-center text-sm font-semibold ">
+            <FaChair/>
+            <span>Furnished</span>
+          </li>)}
+        </ul>
+        </div>
+        <div className="w-full h-[300px] bg-slate-400"></div>
+      </div>
     </main>
   );
 }
